a0a3c13e43875f1c293102aedf001ac6551bc161
krzysiek <kkos8@wp.pl>
2021-04-26 06:03:26 +0200

    Add task 3 - loops

---
+++ script.js
@@ -0,0 +1,21 @@
+let i = 500;
+let para = document.createElement('p');
+
+function isPrime(num) {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+while(i > 1){
+  if(isPrime(i))
+    para.textContent += `${i}, `;
+    i--;
+}
+
+let section = document.querySelector('section');
+section.appendChild(para);
